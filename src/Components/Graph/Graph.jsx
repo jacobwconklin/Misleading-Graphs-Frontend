b2d4bc63952edb957668eefc29f8564b5a58dca3
Jacob Conklin <jacobyconklin@gmail.com>
2023-10-24 10:30:59 -0400

    Added basic functionality to dropdown buttons, don't affect graph yet

--- src/Components/Graph/Graph.jsx
+++ src/Components/Graph/Graph.jsx
@@ -7,54 +7,82 @@ import './Graph.scss';
 // selections users have made for the graph
 const Graph = (props) => {
 
-    // TODO this is just to show how antd dropdowns work, can be deleted
-    const exampleItems = [
-        {
-          key: '1',
-          label: (
-            <p>
-              1st menu item
-            </p>
-          ),
-        },
-        {
-          key: '2',
-          label: (
-            <p>
-              2nd menu item
-            </p>
-          ),
-        },
-        {
-          key: '3',
-          label: (
-            <p>
-              3rd menu item
-            </p>
-          ),
-        },
-    ];
+  const dataSetOptions = [
+      {
+        key: '1',
+        label: "Instant Noodle Sales"
+      },
+      {
+        key: '2',
+        label: "Cat and Dog ownership"
+      },
+      {
+        key: '3',
+        label: "Soccer Stadiums Worldwide"
+      },
+  ];
 
-    return (
-        <div className='Graph'>
-            <h1 className='tryMessage'>Try For Yourself!</h1>
-            <div className='controls'>
-                <Button  
-                    type="primary" 
-                    onClick={() => {alert("Clicked 1")}} 
-                >
-                    Button 1
-                </Button>
-                <div className='buttonSpacer'></div>
-                <Dropdown menu={{ items: exampleItems }} placement="bottomLeft">
-                    <Button>bottomLeft</Button>
-                </Dropdown>
-            </div>
-            <div className='graphDisplay'>
-                <ExampleGraph />
-            </div>
-        </div>
-    )
+  const dataSetOptionsPlusNone = () => {
+    return [{key: '0', label: "NONE"}].concat(dataSetOptions);
+  }
+
+  // handle user choosing a dataset
+  const selectDataset = ({ key }, isFirst) => {
+    // keys corresopnd to the key in the dataSetOptions array above with a NONE option for the
+    // second data set
+    alert("Selected Dataset type: " + dataSetOptionsPlusNone().find(set => set.key === key).label + " for your " + 
+      (isFirst ? "first" : "second" ) + " dataset.");
+  }
+
+  const graphTypes = [
+    {
+      key: '0',
+      label: "Vertical Bar Graph"
+    },
+    {
+      key: '1',
+      label: "Pie Chart"
+    },
+    {
+      key: '2',
+      label: "Scatter Plot"
+    },
+  ];
+
+  // handle user choosing a graph type
+  const selectGraphType = ({ key }) => {
+    // keys corresopnd to the key in the graphType array above
+    alert("Selected graph type: " + graphTypes.find(graph => graph.key === key).label);
+  }
+
+  return (
+      <div className='Graph'>
+          <h1 className='tryMessage'>Try For Yourself!</h1>
+          <div className='controls'>
+              <Dropdown menu={{ items: graphTypes, onClick: selectGraphType }} placement="bottomLeft">
+                  <Button>Graph Type</Button>
+              </Dropdown>
+              <div className='buttonSpacer'></div>
+              <Dropdown menu={{ items: dataSetOptions, onClick: (value => selectDataset(value, true)) }} placement="bottomLeft">
+                  <Button>First Dataset</Button>
+              </Dropdown>
+              <div className='buttonSpacer'></div>
+              <Dropdown menu={{ items: dataSetOptionsPlusNone(), onClick: (value => selectDataset(value, false)) }} placement="bottomLeft">
+                  <Button>Second Dataset</Button>
+              </Dropdown>
+              <div className='buttonSpacer'></div>
+              <Button  
+                  type="primary" 
+                  onClick={() => {alert("Clicked 1")}} 
+              >
+                  Button 1
+              </Button>
+          </div>
+          <div className='graphDisplay'>
+              <ExampleGraph />
+          </div>
+      </div>
+  )
 }
 
 export default Graph;
